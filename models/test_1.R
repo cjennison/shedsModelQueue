148e8e2443ceee841308f4f086633b4df30a05f2
cjennison <cjennison92@gmail.com>
2015-01-12 13:49:34 -0500

    added db and tables to R file

--- models/test_1.R
+++ models/test_1.R
@@ -1,4 +1,5 @@
 library(rjson)
+library(dplyr)
 
 
 #Receive input from user, this should contain 1) working directory, 2) Example or User Basin, 3) Basin Area (km^2)
@@ -11,7 +12,7 @@ print(dir)
 setwd(dir$wd)
 
 
-val <- mean(rnorm(100000))
-
-write(val, dir$output)
-print(val)
+db <- src_postgres(dbname='conte_dev', host='127.0.0.1', port='5432', user='conte', password='conte')
+tbl_locations <- tbl(db, 'locations')
+tbl_values <- tbl(db, 'values')
+tbl_series <- tbl(db, 'series')
