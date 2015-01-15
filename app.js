246cbc163d8b3f9fbbeef6b31573cb2527327886
cjennison <cjennison92@gmail.com>
2015-01-15 08:53:20 -0500

    Added job progression, changed writing to web app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var kue = require('kue'),
     uuid = require('node-uuid'),
     fs = require('fs'),
-    mkdirp = require("mkdirp")
+    mkdirp = require("mkdirp"),
     path = require('path'),
     spawn = require('child_process').spawn;
 
@@ -11,7 +11,6 @@ console.log("Creating Queue")
 
 //process the job
 jobs.process('script', 1, function(job, done){
-  console.log("[Queue] > Processing script");
 
   //Parse Job Data
   if(typeof(job.data) == "string")
@@ -20,32 +19,18 @@ jobs.process('script', 1, function(job, done){
   //Check if the job is valid to be run
   if(!validateJob(job.data)) return done(new Error('invalid job'));
 
-  // console.log(job)
-  var dir = uuid.v4();
-  var local_directory = "runs/" + dir;
-  mkdirp(local_directory, function(err){
-    if(err) console.log(err)
-    else {
-      console.log(dir + ": created")
-
-
-      //Write JSON file
-      writeJSON(job, "runs/" + dir + "/", function(){
-        runModel(job, local_directory, done);
-      });
-    }
-  })
-
+  runModel(job, job.data.wd, done);
   
 })
 
 //Spawns the R script to run the job
 function runModel(job, dir, cb){
+  
   var script = job.data.model_script;
 
   //Create the opts for spawn
   var opts = {
-    cwd: __dirname + "/" + dir + "/",
+    cwd: dir,
     env: process.env
   }
 
@@ -60,19 +45,18 @@ function runModel(job, dir, cb){
   var args = [ __dirname + "/models/" + script, rargs ];
 
   //Spawn the session
-  var proc = spawn("Rscript", [__dirname + "/models/" + script, __dirname + "/" + dir + "/", 'input.json', 'output.json'], opts);
+  job.progress(0); // -- set progress
+  console.log(job)
+  var proc = spawn("Rscript", [__dirname + "/models/" + script, dir, 'input.json', 'output.json'], opts);
 
   //Watch for data 
   proc.stdout.on('data', function(data){
-    console.log("stdout: " + data);
+    //console.log("stdout: " + data);
   })
 
   proc.stderr.on('data', function(data){
-    console.log("stderr: " + data);
+    //console.log("stderr: " + data);
   })
-
-  job.data.output = "I'm some output :D"
-  job.save();
 
   //Check Exit Code
   proc.on('exit', function(code){
@@ -84,18 +68,6 @@ function runModel(job, dir, cb){
     cb();
   })
 
-}
-
-//Write the JSON input field
-function writeJSON(job, dir, cb){
-  console.log("[writeJSON] > Writing JSON File")
-  console.log(job.data)
-  fs.writeFile(dir + "input.json", JSON.stringify(job.data.input, null), function(err){
-    if(err) console.log(err)
-    else {
-      cb();
-    }
-  })
 }
 
 //Make sure a job is valid
